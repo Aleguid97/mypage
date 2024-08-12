e9abc093f733705ad2fef545d31ec95af374c1b3
Aleguid97 <aleguid97@gmail.com>
2024-08-12 15:45:37 +0200

    tanto non lo puoi fare

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Cards from './Components/Cards.jsx';
-import Aboutme from './Components/Aboutme.jsx';
-import { useMediaQuery } from 'react-responsive';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Cards from "./Components/Cards.jsx";
+import Aboutme from "./Components/Aboutme.jsx";
+import { useMediaQuery } from "react-responsive";
+import "./App.css";
 
 function App() {
-  const isTabletOrMobile = useMediaQuery({ query: '(min-width: 300px) and (max-width: 768px)' });
-  
-  const showAboutMe = () => {
-    const aboutme = document.querySelector('.aboutme');
-    aboutme.style.display = 'block'; 
-  }
+  const isTabletOrMobile = useMediaQuery({ query: "(min-width: 300px) and (max-width: 768px)" });
+
+  const toggleAboutMe = () => {
+    const aboutme = document.querySelector(".aboutme");
+
+    if (aboutme.style.display === "none") {
+      aboutme.style.display = "block";
+    } else {
+      aboutme.style.display = "none";
+    }
+  };
 
   return (
     <Router>
       <div className="App container-fluid">
         <div className="row">
-          <div className={`col-12 col-md-6 ${isTabletOrMobile ? 'order-1' : ''}`}>
+          <div className={`col-12 col-md-6 ${isTabletOrMobile ? "order-1" : ""}`}>
             <Cards />
           </div>
 
-          <div className={`col-12 col-md-6 ${isTabletOrMobile ? 'order-2' : ''}`}>
+          <div className={`col-12 col-md-6 ${isTabletOrMobile ? "order-2" : ""}`}>
             {isTabletOrMobile && (
               <div className="buttons-container text-center my-2">
-                <button className="btn-download rounded-pill btn btn-primary" onClick={showAboutMe}>About Me
-                  </button>      
+                <button className="btn-download rounded-pill btn btn-primary" onClick={toggleAboutMe}>
+                  About Me
+                </button>
               </div>
             )}
             <Aboutme />
@@ -36,6 +42,5 @@ function App() {
     </Router>
   );
 }
-
 
 export default App;
